5ca5a5340d36efb5685e19100c745683a0141f1a
NavarroKofs <kofsmorrizon@gmail.com>
2022-08-03 18:48:19 -0500

    cors problem with petitions fixed

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,30 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-    babel: {
-      plugins: ['@emotion/babel-plugin']
-    }
-  })],
-  server: {
-    host: true,
-    proxy: {
-      '/apiKonami': {
-        target: 'https://img.yugioh-card.com/en/downloads/forms/KDE_DeckList.pdf',
-        changeOrigin: true,
-        secure: false,
-        rewrite: path => path.replace('/apiKonami', '')
-      },
-      '/api': {
-        target: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
-        changeOrigin: true,
-        secure: false,
-        rewrite: path => path.replace('/api', '')
+export default defineConfig(({ command, mode }) => {
+  const isDev = mode === 'development'
+  return {
+    plugins: [react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
+    })],
+    server: {
+      host: true,
+      proxy: {
+        '/apiKonami': {
+          target: 'https://img.yugioh-card.com/en/downloads/forms/KDE_DeckList.pdf',
+          changeOrigin: isDev,
+          secure: !isDev,
+          rewrite: path => path.replace('/apiKonami', '')
+        },
+        '/api': {
+          target: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+          changeOrigin: isDev,
+          secure: !isDev,
+          rewrite: path => path.replace('/api', '')
+        }
       }
     }
   }
